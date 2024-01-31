95a3480ad47aa090af8a53572e19ba8381cf7c7d
YangHongwei <403236160@qq.com>
2024-01-31 15:18:06 +0800

    feat: zipkin简易使用与调用

--- src/main/java/com/sxqibo/spring/TestController.java
+++ src/main/java/com/sxqibo/spring/TestController.java
@@ -1,13 +1,17 @@
 package com.sxqibo.spring;
 
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
     @GetMapping("/test")
+    @NewSpan
     public String test() {
-        return "test";
+        return "Hello from Microservice A!";
     }
 }
